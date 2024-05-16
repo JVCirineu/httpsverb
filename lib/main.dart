d0f90a86a87837c23d443f4d71b3180b9d9b2305
JVCirineu <132360356+JVCirineu@users.noreply.github.com>
2024-05-16 11:47:27 -0300

    httpverbo

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutterhttps/Principal.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home: Principal(),
+));
+
+
